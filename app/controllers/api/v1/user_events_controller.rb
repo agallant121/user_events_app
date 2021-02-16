082cc98493aedccb64d7f4bfe12a72dc10d2cf5f
Alex Gallant <agallant1201@gmail.com>
2021-02-16 11:10:19 -0700

    Add two more specs and refactor user_events controller and spec to DRY it up

--- app/controllers/api/v1/user_events_controller.rb
+++ app/controllers/api/v1/user_events_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UserEventsController < ApplicationController
+    before_action :information, only: [:create, :remove_event_type, :inc_event_type, :inc_name]
+
     def index
         render json: UserEvent.all
     end
@@ -7,31 +9,44 @@ class Api::V1::UserEventsController < ApplicationController
         render json: UserEvent.find(params[:id])
     end
 
-    def create
-        info = params[:event]
-        
-        if info.keys.include?('event_type') && info.keys.include?('name')
-
-            user_event = UserEvent.new(event_type: info['event_type'], event_data: remove_event_type(params))
+    def create        
+        if inc_event_type && inc_name
+            user_event = UserEvent.new(event_type: information['event_type'], event_data: remove_event_type)
             user_event.save
-            render json: {status: "success", code: 200, message: "User Event Created"}
+            json_message('success', 200, 'User Event Created')
         elsif 
-            !info.keys.include?('event_type') && !info.keys.include?('name')
-            render json: {status: "error", code: 422, message: "Missing Keys: 'event_type' and 'name'"}
+            !inc_event_type && !inc_name
+            json_message('error', 422, 'Missing Keys: event_type and name')
         elsif 
-            !info.keys.include?('event_type') && info.keys.include?('name')
-            render json: {status: "error", code: 422, message: "Missing Keys: 'event_type'"}
+            !inc_event_type && inc_name
+            json_message('error', 422, 'Missing Keys: event_type')
+
         else 
-            info.keys.include?('event_type') && !info.keys.include?('name')
-            render json: {status: "error", code: 422, message: "Missing Keys: 'name'"}
+            inc_event_type && !inc_name
+            json_message('error', 422, 'Missing Keys: name')
         end
     end
 
     private
 
-    def remove_event_type(params)
-        info = params['event']
-        info.delete('event_type')
-        info
+    def remove_event_type
+        information.delete('event_type')
+        information
+    end
+
+    def inc_event_type
+        information.keys.include?('event_type')
+    end
+
+    def inc_name
+        information.keys.include?('name')
+    end
+
+    def json_message(status, code, message)
+        render status: "#{code}", json: {code: "#{status}", status: "#{code}", message: "#{message}"}
+    end
+
+    def information
+        params['event']
     end
 end
--- spec/requests/api/v1/user_event_request_spec.rb
+++ spec/requests/api/v1/user_event_request_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 describe "User Events API" do
     before :each do 
         UserEvent.destroy_all
+        @header = { "CONTENT_TYPE" => "application/json" }
     end
+
     
     it "can get all user events" do
         create_list(:user_event, 5)
@@ -28,25 +30,50 @@ describe "User Events API" do
     end
 
     it "can create a new user event with necessary keys" do
-        headers = { "CONTENT_TYPE" => "application/json" }
 
-        post "/api/v1/user_events", params: '{"event" : {"name" : "test button", "event_type" : "click",    "at" : "2020-06-12T00:00:01", "button_color" : "red" }}', headers: headers
+        post "/api/v1/user_events", params: '{"event" : {"name" : "test button", "event_type" : "click",    "at" : "2020-06-12T00:00:01", "button_color" : "red" }}', headers: @header
 
         event = UserEvent.last
 
         expect(response).to be_successful
+        expect(response).to have_http_status(200)
         expect(UserEvent.count).to eq(1)
         expect(event.event_type).to eq('click')
     end
 
-    it "cannot create new user event without necessary keys" do
-        headers = { "CONTENT_TYPE" => "application/json" }
+    it "cannot create new user event without necessary name key" do
 
-        post "/api/v1/user_events", params: '{"event" : {"wrong_key" : "test button", "event_type" : "click",    "at" : "2020-06-12T00:00:01", "button_color" : "red" }}', headers: headers
+        post "/api/v1/user_events", params: '{"event" : {"wrong_key" : "test button", "event_type" : "click",    "at" : "2020-06-12T00:00:01", "button_color" : "red" }}', headers: @header
 
-        event = UserEvent.last
-        
-        expect(response).to be_successful
+        body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to_not be_successful
+        expect(response).to have_http_status(422)
         expect(UserEvent.count).to eq(0)
+        expect(body[:message]).to eq("Missing Keys: name")
+    end
+
+    it "cannot create new user event without necessary event_type key" do
+
+        post "/api/v1/user_events", params: '{"event" : {"name" : "test button", "wrong_key" : "click",    "at" : "2020-06-12T00:00:01", "button_color" : "red" }}', headers: @header
+
+        body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to_not be_successful
+        expect(response).to have_http_status(422)
+        expect(UserEvent.count).to eq(0)
+        expect(body[:message]).to eq("Missing Keys: event_type")
+    end
+
+    it "cannot create new user event without necessary event_type and name keys" do
+
+        post "/api/v1/user_events", params: '{"event" : {"wrong_key1" : "test button", "wrong_key2" : "click",    "at" : "2020-06-12T00:00:01", "button_color" : "red" }}', headers: @header
+
+        body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to_not be_successful
+        expect(response).to have_http_status(422)
+        expect(UserEvent.count).to eq(0)
+        expect(body[:message]).to eq("Missing Keys: event_type and name")
     end
 end
